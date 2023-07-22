d3ec8b73afb05566c9974067a89b6184840d0271
ryzhan43 <ryzhan43@gmail.com>
2023-07-21 20:50:40 -0400

    init

---
+++ _00-BigONotion/src/main/java/org/example/TimeTest.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class TimeTest {
+    public static void main(String[] args) {
+        int n=10000;
+        long startTime=System.currentTimeMillis();
+        int numberOfOperations=0;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n;j++) numberOfOperations++;
+        }
+        long endTime=System.currentTimeMillis();
+        System.out.println("Number of Operations is = "+ numberOfOperations + " in "+ (endTime-startTime)+" milisecs");
+    }
+}
