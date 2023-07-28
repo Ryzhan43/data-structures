fa66ecca1932f24229a2beead3ba61d1b21b34e5
ryzhan43 <ryzhan43@gmail.com>
2023-07-28 14:00:15 -0400

    Singly Linked List

--- _02-LinkedList/src/main/java/org/example/LinkedListApp.java
+++ _02-LinkedList/src/main/java/org/example/LinkedListApp.java
@@ -6,9 +6,14 @@ public class LinkedListApp {
 
     public static void main(String[] args) {
         MySinglyLinkedList list1 = new MySinglyLinkedList();
-        for(int i=0;i<=5;i++) list1.add(i);
+        int[] array = {1,1,1,2,3,4,5,5,5,5,5,5};
+        for(int i=0;i<=array.length-1;i++){ list1.add(array[i]);}
         list1.printNodes();
-
-        System.out.println(list1.indexOf(4));
+        list1.removeDuplicates2();
+        list1.printNodes();
+//        System.out.println("///////////////");
+//        list1.reverse();
+//        list1.printNodes();
+//        System.out.println(list1.indexOf(4));
     }
 }
