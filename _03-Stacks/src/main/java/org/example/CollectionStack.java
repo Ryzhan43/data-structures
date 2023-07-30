f5d5c1933bb95d14f93273b22350701d4ee6f853
ryzhan43 <ryzhan43@gmail.com>
2023-07-30 19:58:47 -0400

    Expression Balance check

---
+++ _03-Stacks/src/main/java/org/example/CollectionStack.java
@@ -0,0 +1,25 @@
+package org.example;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.Stack;
+
+public class CollectionStack {
+    public static void main(String[] args) {
+        Deque<Integer> sStack=new ArrayDeque<>();
+        Deque<Integer> sStack1=new LinkedList<>();
+        Stack<Integer> stack = new Stack<>();
+        for (int i = 0; i < 5; i++) {
+            stack.push(i);
+        }
+        System.out.println("Stack elements are:");
+        stack.forEach(System.out::println);
+        System.out.println("Top of the stack is:" + stack.peek());
+        stack.pop();
+        stack.pop();
+        System.out.println("Updated Stack:    ");
+        stack.forEach(System.out::println);
+
+    }
+}
