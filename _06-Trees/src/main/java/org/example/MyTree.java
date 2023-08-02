60e6778f9c072cfbe74efb9be6a069731ea03eb0
ryzhan43 <ryzhan43@gmail.com>
2023-08-02 15:00:40 -0400

    Trees added

--- _06-Trees/src/main/java/org/example/MyTree.java
+++ _06-Trees/src/main/java/org/example/MyTree.java
@@ -1,5 +1,8 @@
 package org.example;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class MyTree {
     public Node root;
 
@@ -58,4 +61,47 @@ public class MyTree {
         postOrderTraversal(root.rightChild);
         System.out.print(root.value+", ");
     }
+
+    public void levelOrderTraversal(){
+        if(root==null) return;
+        Queue<Node> queue = new LinkedList<>();
+        queue.add(root);
+        while (!queue.isEmpty()){
+            Node toVisit= queue.poll();
+            System.out.print(toVisit.value+", ");
+            if (toVisit.leftChild!=null){
+                queue.add(toVisit.leftChild);
+            }
+            if (toVisit.rightChild!=null){
+                queue.add(toVisit.rightChild);
+            }
+        }
+    }
+
+// Task - 1: if tree contains the function
+    public boolean contains(int value){
+
+        if(root==null) return false;
+        Node current=root;
+        while (current!=null){
+            if(value< current.value)current=current.leftChild;
+            else if (value> current.value)current=current.rightChild;
+            else return true;
+        }
+        return false;
+    }
+
+// Task - 2: is it a leaf?
+    public boolean isLeaf(Node node){
+        return node.rightChild==null && node.leftChild==null;
+    }
+
+// Task - 3: print the leafs
+    public void printLeafs(Node node){
+        if(root==null) return;
+        if(isLeaf(root)) System.out.println(root.value+", ");
+        printLeafs(root.leftChild);
+        printLeafs(root.rightChild);
+    }
+
 }
--- _06-Trees/src/main/java/org/example/TreeApp.java
+++ _06-Trees/src/main/java/org/example/TreeApp.java
@@ -18,8 +18,8 @@ public class TreeApp {
         System.out.println("Post-Order Traversal");
         tree.postOrderTraversal(tree.root);
         System.out.println();
-//        System.out.println("Breadth First Traversal");
-//        tree.levelOrderTraversal();
+        System.out.println("Breadth First Traversal");
+        tree.levelOrderTraversal();
 
         /*System.out.println();
         System.out.println("tree contains 6 is : " + tree.contains(6));
