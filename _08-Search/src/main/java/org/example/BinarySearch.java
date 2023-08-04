62f38ed0f1fe426cfe975096de13b9302eae1610
ryzhan43 <ryzhan43@gmail.com>
2023-08-04 16:11:20 -0400

    BinarySearch added

--- _08-Search/src/main/java/org/example/BinarySearch.java
+++ _08-Search/src/main/java/org/example/BinarySearch.java
@@ -12,4 +12,18 @@ public class BinarySearch {
         }
         return -1;
     }
+    public static int binarySearchRecursive(int[] array, int data){
+        return binarySearchRecursive(array,data,0, array.length-1);
+    }
+    public static int binarySearchRecursive(int[] array, int data, int left, int right){
+        while(right<left){
+            int middle=(left+right)/2;
+            if(array[middle]==data) return middle;
+            if(data<array[middle]) binarySearchRecursive(array, data,left,middle-1);
+            else binarySearchRecursive(array, data,middle+1, right);
+        }
+        return -1;
+    }
+
+
 }
